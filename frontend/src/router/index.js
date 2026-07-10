abd1a505a61fa26769b1e7ebac814dab94b5f072
xcjlms <484901562@qq.com>
2026-07-10 12:22:57 +0800

    Refactor router configuration for clarity

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,100 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/login',
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
-  },
-  {
-    path: '/home/reader',
-    name: 'HomeReader',
-    component: () => import('../views/HomeReader.vue'),
-  },
-  {
-    path: '/home/admin',
-    name: 'HomeAdmin',
-    component: () => import('../views/HomeAdmin.vue'),
-  },
-  {
-    path: '/borrow',
-    name: 'Borrow',
-    component: () => import('../views/Borrow.vue'),
-  },
-  {
-    path: '/back',
-    name: 'Back',
-    component: () => import('../views/Back.vue'),
-  },
-  {
-    path: '/fine',
-    name: 'Fine',
-    component: () => import('../views/Fine.vue'),
-  },
-  {
-    path: '/admin/books',
-    name: 'BookManage',
-    component: () => import('../views/BookManage.vue'),
-  },
-  {
-    path: '/admin/users',
-    name: 'UserManage',
-    component: () => import('../views/UserManage.vue'),
-  },
-  {
-    path: '/admin/feedback',
-    name: 'FeedbackManage',
-    component: () => import('../views/FeedbackManage.vue'),
-  },
-  {
-    path: '/admin/backup',
-    name: 'Backup',
-    component: () => import('../views/Backup.vue'),
-  },
-  {
-    path: '/reader/feedback',
-    name: 'FeedbackSubmit',
-    component: () => import('../views/FeedbackSubmit.vue'),
-  },
-  {
-    path: '/reader/profile',
-    name: 'Profile',
-    component: () => import('../views/Profile.vue'),
-  },
-  {
-    path: '/admin/books',
-    name: 'BookManage',
-    component: () => import('../views/BookManage.vue'),
-  },
-  {
-    path: '/admin/users',
-    name: 'UserManage',
-    component: () => import('../views/UserManage.vue'),
-  },
-  {
-    path: '/admin/feedback',
-    name: 'FeedbackManage',
-    component: () => import('../views/FeedbackManage.vue'),
-  },
-  {
-    path: '/admin/backup',
-    name: 'Backup',
-    component: () => import('../views/Backup.vue'),
-  },
-  {
-    path: '/reader/feedback',
-    name: 'FeedbackSubmit',
-    component: () => import('../views/FeedbackSubmit.vue'),
-  },
-  {
-    path: '/reader/profile',
-    name: 'Profile',
-    component: () => import('../views/Profile.vue'),
-  },
+  { path: '/', redirect: '/login' },
+  { path: '/login', name: 'Login', component: () => import('../views/Login.vue') },
+  { path: '/home/reader', name: 'HomeReader', component: () => import('../views/HomeReader.vue') },
+  { path: '/home/admin', name: 'HomeAdmin', component: () => import('../views/HomeAdmin.vue') },
+  { path: '/borrow', name: 'Borrow', component: () => import('../views/Borrow.vue') },
+  { path: '/back', name: 'Back', component: () => import('../views/Back.vue') },
+  { path: '/fine', name: 'Fine', component: () => import('../views/Fine.vue') },
+  { path: '/admin/books', name: 'BookManage', component: () => import('../views/BookManage.vue') },
+  { path: '/admin/users', name: 'UserManage', component: () => import('../views/UserManage.vue') },
+  { path: '/admin/feedback', name: 'FeedbackManage', component: () => import('../views/FeedbackManage.vue') },
+  { path: '/admin/backup', name: 'Backup', component: () => import('../views/Backup.vue') },
+  { path: '/reader/feedback', name: 'FeedbackSubmit', component: () => import('../views/FeedbackSubmit.vue') },
+  { path: '/reader/profile', name: 'Profile', component: () => import('../views/Profile.vue') },
 ]
 
 const router = createRouter({
